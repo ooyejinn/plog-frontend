13336a94f4f37d5a77341beb37e9665abe7854cb
오예진 <ooyejinn@gmail.com>
2024-07-29 17:14:04 +0900

    feat: Add Plant Components1

---
+++ src/components/Plant/PlantDetailPage.js
@@ -0,0 +1,5 @@
+import PlantProfileHeader from "./PlantProfileHeader";
+import PlantCalander from "./PlantCalander";
+// 일지 모아보기 list common? sns? 에서 불러오기
+import PlantAnalysisBanner from "./PlantAnalysisBanner";
+// 버튼 common에서 불러오기 (성장과정 btn, 가이드 btn)
