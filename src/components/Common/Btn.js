0fccef75a087b8effaf8b88801a6e39c6ec81344
c4526 <c4526@naver.com>
2024-07-30 12:42:52 +0900

    feat: Add common component

--- src/components/Common/Btn.js
+++ src/components/Common/Btn.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Btn = ({ content, onClick }) => {
-    return (
-      <button
-        onClick={onClick}
-      >
-        {children}
-      </button>
-    );
-  };
+const Btn = ({ content, onClick, style, disabled }) => {
+  return (
+    <button onClick={onClick} style={style} disabled={disabled}>
+      {content}
+    </button>
+  );
+};
 
-  
+Btn.propTypes = {
+  content: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  style: PropTypes.object,
+  disabled: PropTypes.bool,
+};
+
+
+export default Btn;
