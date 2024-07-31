cdea19f6c0aed69943f3c9667fb47357ef9afff1
진아영 <ay8160@daum.net>
2024-08-01 00:17:06 +0900

    feat: setting과 연결된 페이지 navigate 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProfileUpdate from './pages/Account/ProfileUpdate';
+import PasswordUpdate from './pages/Account/PasswordUpdate';
 import Setting from './pages/Account/Setting';
+
 
 function App() {
   return (
-    <div className="App">
-      <Setting />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Setting />} />
+        <Route path="/profile-update" element={<ProfileUpdate />} />
+        <Route path="/password-update" element={<PasswordUpdate />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/pages/Account/Setting.js
+++ src/pages/Account/Setting.js
@@ -1,20 +1,20 @@
 import React from "react";
-import './Setting.css';
+import { useNavigate } from "react-router-dom";
 
 const Setting = () => {
+  const navigate = useNavigate();
 
   return (
     <div className="settings-container">
       <section className="settings-section">
         <h2>개인정보</h2>
-        <div className="settings-item">회원정보 수정</div>
-        <div className="settings-item">비밀번호 변경</div>
+        <div className="settings-item" onClick={() => navigate('/password-update')}>비밀번호 변경</div>
+        <div className="settings-item" onClick={() => navigate('/profile-update')}>회원정보 수정</div>
         <div className="settings-item">서비스 이용 동의</div>
       </section>
       <section className="settings-section">
         <h2>일반 설정</h2>
         <div className="settings-item">푸시 알림 설정</div>
-        <div className="settings-item">뭐시기 설정</div>
       </section>
       <section className="settings-section">
         <h2>카테고리</h2>
