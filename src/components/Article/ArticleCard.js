736f0f559dabcaf1a747a344b31d464f4d98dc49
오예진 <ooyejinn@gmail.com>
2024-07-31 09:21:32 +0900

    docs: 읽는 사람을 고려해 주석 보완

--- src/components/Article/ArticleCard.js
+++ src/components/Article/ArticleCard.js
@@ -3,6 +3,9 @@ import React from 'react';
 // id는 눌렀을 때 detail 페이지로 연결하는 데에 씀
 // 일지의 경우 일지 detail, sns의 경우 sns detail
 
+// id : ArticleCard 컴포넌트를 눌렀을 때에 detail 페이지로 연결하는 데에 사용
+// 일지의 경우 plantDiaryId, sns의 경우 articleId
+
 const ArticleCard = ({ id, log, thumbnail }) => {
   return (
     <div>
