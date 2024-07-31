50a7bb3e176ee55754c24f7c605d14fa01ba34ff
c4526 <c4526@naver.com>
2024-07-31 10:38:12 +0900

    refactor: 가이드 식물이름 변수 수정

--- src/pages/Diary/PlantGuide.js
+++ src/pages/Diary/PlantGuide.js
@@ -5,15 +5,16 @@ import DiaryDetailContent from '../../components/Diary/DiaryDetailContent';
 const PlantGuide = () => {
   //임시 데이터
   const plantData = {
-    plantTypeId: 1, //궁금한 점이 이게 종 맞나?1번에 넣을 값 이름이 몬스테라 인건가?
+    plantTypeId: 1,
     otherPlantTypeId: 0,
+    plantName: '몬스테라',
     profile: 'https://via.placeholder.com/300',
     info: '몬스테라는 병충해 어쩌구 저쩌구' 
   };
 
   return (
     <div>
-      <h2>{plantData.plantTypeId} 키우기</h2>
+      <h2>{plantData.plantName} 키우기</h2>
       <Img src={plantData.profile} alt={`이미지`} />
       <DiaryDetailContent detailContent={plantData.info} />
     </div>
