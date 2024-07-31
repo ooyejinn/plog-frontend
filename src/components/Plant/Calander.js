a27b98648416cd9ee88af56921b4bfdf5ea68b3c
오예진 <ooyejinn@gmail.com>
2024-07-31 09:30:50 +0900

    docs: Calander, PlantPanorama에서 이후 구현해야할 것 고려해 주석 추가

--- src/components/Plant/Calander.js
+++ src/components/Plant/Calander.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+// react-calendar 라이브러리 활용하기
+// get 요청을 URI에 보낸 뒤 (관리 기록과 일지 기록),
+// 응답 데이터가 있다면 상태 변화시키고 CSS 변화시키기
+// 캘린더를 월 기준으로 지난달을 보게 할 수 있어야 하는데
+// render가 다시 된다는 점 고려해서 상태 관리하기
+
 const Calander = () =>{
   return (
     <div>
