da42c85f68a12aee7867d29ad665f1345a8dcdf2
진아영 <ay8160@daum.net>
2024-07-31 17:26:13 +0900

    feat: 설정 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import PasswordFind from './pages/Account/PasswordFind';
-import SignUp from './pages/Account/SignUp';
-import Login from './pages/Account/Login';
-import PasswordUpdate from './pages/Account/PasswordUpdate';
-import ProfileUpdate from './pages/Account/ProfileUpdate';
+import Setting from './pages/Account/Setting';
 
 function App() {
   return (
     <div className="App">
- 
+      <Setting />
     </div>
   );
 }
---
+++ src/pages/Account/Setting.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Setting = () => {
+
+  return (
+    <div>
+      <h1>Settings</h1>
+      <p>This is the Settings page.</p>
+    </div>
+  );
+}
+
+export default Setting;
