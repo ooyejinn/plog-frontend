42c544b100ef907b2083c89679bf0e5437cae56f
진아영 <ay8160@daum.net>
2024-08-01 15:02:12 +0900

    chore: axios, js-sha256 설치

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProfileUpdate from './pages/Account/ProfileUpdate';
 import PasswordUpdate from './pages/Account/PasswordUpdate';
-import Setting from './pages/Account/Setting';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Account/Login';
 import PlantDetail from './pages/Plant/PlantDetail';
 
 function App() {
