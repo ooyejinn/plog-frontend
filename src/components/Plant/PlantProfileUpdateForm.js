20556eb65c515e15c2bb90feea66f13386740ec7
오예진 <ooyejinn@gmail.com>
2024-08-02 13:39:16 +0900

    feat: ProfileUpdateForm 참고하여 PlantProfileUpdateForm 페이지 컴포넌트 작성중2

--- src/components/Plant/PlantProfileUpdateForm.js
+++ src/components/Plant/PlantProfileUpdateForm.js
@@ -5,6 +5,13 @@ import ProfileImgEdit from '../../components/Profile/ProfileImgEdit';
 
 
 const PlantProfileUpdateForm = ({ plantData }) => {
+  // 종, 닉네임, 생일, 소개, 이미지
+  /* TODO: otherPlantTypeId일 경우도 추가 */
+  const [plantTypeId, setPlantTypeId] = useState(plantData.plantTypeId);
+  const [nickname, setNickname] = useState(plantData.nickname);
+  const [birthDate, setBirthDate] = useState(plantData.birthDate);
+  const [bio, setBio] = useState(plantData.bio);
+  const [profile, setProfile] = useState(plantData.profile);
 
   const [isFormValid, setIsFormValid] = useState(false);
 
