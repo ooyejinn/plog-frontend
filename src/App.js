1551b387d8c6292047b9cebdff54c507e4bd49af
c4526 <c4526@naver.com>
2024-08-04 00:09:36 +0900

    feat: SnsWrite 페이지 Tab, 사진, 내용, 버튼 추가

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PlantDiaryWrite from './pages/Diary/PlantDiaryWrite';
 import PlantDiaryDetail from './pages/Diary/PlantDiaryDetail';
 import PlantDetail from './pages/Plant/PlantDetail';
-
+import SnsWrite from './pages/Sns/SnsWrite';
 function App() {
   return (
     <Router>
     <Routes>
-      <Route path="/" element={<PlantDetail />} />
+      <Route path="/" element={<SnsWrite />} />
       <Route path="/write" element={<PlantDiaryWrite />} />
       <Route path="/diary/:plantDiaryId" element={<PlantDiaryDetail />} />
     </Routes>
--- src/pages/Sns/SnsWrite.js
+++ src/pages/Sns/SnsWrite.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ImgUpload from '../../components/Common/ImgUpload';
-import cameraIcon from '../../assets/icon/camera.png';
 import Btn from '../../components/Common/Btn';
 import Tab from '../../components/Sns/Tab';
+import Content from '../../components/Common/Content';
+
+import cameraIcon from '../../assets/icon/camera.png';
 
 const SnsWrite = () => {
   const [imgs, setImgs] = useState([]);
+  const [content, setContent] = useState('');
 
   const handleImageUpload = (event) => {
     const files = Array.from(event.target.files);
@@ -23,15 +26,21 @@ const SnsWrite = () => {
   };
 
   return (
-    
     <div>
-      <Tab />
-      <ImgUpload 
-        cameraIcon={cameraIcon} 
-        imgs={imgs} 
-        handleImageUpload={handleImageUpload} 
-        handleDeleteImage={handleDeleteImage} 
-      />
+      <div>
+        <Tab />
+      </div>
+      <div>
+        <ImgUpload 
+          cameraIcon={cameraIcon} 
+          imgs={imgs} 
+          handleImageUpload={handleImageUpload} 
+          handleDeleteImage={handleDeleteImage} 
+        />
+      </div>
+      <div>
+        <Content content={content} setContent={setContent} />
+      </div>
       <div>
         <Btn content="작성하기" onClick={() => console.log('작성하기 버튼 클릭')} />
       </div>
