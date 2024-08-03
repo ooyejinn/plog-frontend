596dd227ebe9b1d9485c52bcc016f721d096d381
c4526 <c4526@naver.com>
2024-08-04 02:33:48 +0900

    feat: 일지 작성 페이지 코드 수정 및 API 추가

--- src/pages/Diary/PlantDiaryWrite.js
+++ src/pages/Diary/PlantDiaryWrite.js
@@ -14,21 +14,15 @@ import fertilizedIcon from '../../assets/icon/fertilized.png';
 import repottedIcon from '../../assets/icon/repotted.png'; 
 import './PlantDiaryWrite.css';
 
-const PlantDiaryWrite = ({ currentDate = new Date(), plantId }) => { 
-  //임시데이터
-  // const currentDate = new Date();  
-  // const plantId = '1'; 
-  const [diaries, setDiaries] = useState([]);
+const PlantDiaryWrite = ({ currentDate = new Date(), plantId }) => {  
   const [content, setContent] = useState('');
   const [date, setDate] = useState(currentDate.toISOString().slice(0, 10)); 
   const [isWatered, setIsWatered] = useState(false);
   const [isFertilized, setIsFertilized] = useState(false);
   const [isRepotted, setIsRepotted] = useState(false);
   const [imgs, setImgs] = useState([]);
-  // const [isExistingDiary, setIsExistingDiary] = useState(false); // 현재 날짜에 이미 작성된 일지가 있는지 여부
-  // const [showConfirmation, setShowConfirmation] = useState(false); //날짜 변경 시 이미 작성된 일지가 있을 경우, 날짜 변경 여부를 확인하는 팝업을 표시
   const [plantDiaryId, setplantDiaryId] = useState(null); //현재 날짜에 이미 작성된 일지가 있을 경우 해당 일지의 ID를 저장
-
+  const [isEditMode, setIsEditMode] = useState(false);
 
   const navigate = useNavigate();
 
@@ -42,24 +36,26 @@ const PlantDiaryWrite = ({ currentDate = new Date(), plantId }) => {
   const humidity = 3;
   const temperature = 30.0;
 
+
   // 해당 날짜에 작성된 일지 확인 
-  const fetchDiary = async ({checkDate, plantId}) => {
+  const fetchDiary = async ({recordDate='2024-08-02', plantId=1}) => {
     try {
-      const response = await fetch(`/api/user/plant/${plantId}/diary?checkDate=${checkDate}`, {
+      const diaryResponse = await fetch(`https://i11b308.p.ssafy.io/api/user/plant/${plantId}/diary?recordDate=${recordDate}`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
+          // 'Authorization': ,
         },
       });
       
-      // if (!response.ok) {
-      //   throw new Error('일지 조회에 실패했습니다.');
-      // }
+      if (!diaryResponse.ok) {
+        throw new Error('일지 조회에 실패했습니다.');
+      }
 
       // 처음 페이지 로딩했을 때, 해당 날짜의 일지가 있으면 content 등등 그 내용으로 업로드
-      const data = await response.json();
-      console.log(data);
-      return data;
+      const dataDiary = await diaryResponse.json();
+      console.log(dataDiary);
+      return dataDiary;
 
     } catch (error) {
       console.error('Error:', error);
@@ -71,7 +67,7 @@ const PlantDiaryWrite = ({ currentDate = new Date(), plantId }) => {
   // 해당 날짜에 작성된 관리 기록 확인
   const fetchPlantCheck = async (checkDate, plantId) => {
     try {
-      const response = await fetch(`/api/user/plant/${plantId}/check?checkDate=${checkDate}`, {
+      const plantResponse = await fetch(`https://i11b308.p.ssafy.io/api/user/plant/${plantId}/check?checkDate=${checkDate}`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -82,9 +78,9 @@ const PlantDiaryWrite = ({ currentDate = new Date(), plantId }) => {
       // if (!response.ok) {
       //   throw new Error('관리 기록 조회에 실패했습니다.');
       // }
-      const data = await response.json();
-      console.log(data);
-      return data;
+      const dataPlant = await plantResponse.json();
+      console.log(dataPlant);
+      return dataPlant;
     } catch (error) {
       console.error('Error:', error);
       console.log('새로운 일지를 작성합니다.');
@@ -97,7 +93,7 @@ const PlantDiaryWrite = ({ currentDate = new Date(), plantId }) => {
   useEffect(() => {
     const getDiaryAndPlantCheck = async () => {
       try {
-        const diary = await fetchDiary({checkDate: date, plantId});
+        const diary = await fetchDiary({recordDate: date, plantId});
         const plantCheck = await fetchPlantCheck({checkDate: date, plantId});
         if (diary || plantCheck) {
           alert('이미 작성된 일기가 있습니다.');
@@ -119,7 +115,7 @@ const PlantDiaryWrite = ({ currentDate = new Date(), plantId }) => {
       }
     };
     getDiaryAndPlantCheck();
-  }, [date]);
+  }, [date, plantId]);
 
 
   // 정보 입력
@@ -164,7 +160,7 @@ const PlantDiaryWrite = ({ currentDate = new Date(), plantId }) => {
       humidity,
       content,
       image: imgs.map((img, index) => ({
-        url: img,
+        url: img.url,
         isThumbnail: index === 0 // 첫 번째 이미지를 대표 사진으로 설정
       })),
       recordDate: date,
@@ -174,66 +170,120 @@ const PlantDiaryWrite = ({ currentDate = new Date(), plantId }) => {
       isWatered,
       isFertilized,
       isRepotted,
-      checkDate: date
+      checkDate: date,
     }
-
-    // 일지 작성 요청
+    // 일지작성요청 2
     try {
-      const response = await fetch(
-        plantDiaryId ? `/api/user/diary/${plantDiaryId}` : '/api/user/diary', 
+      const diaryWriteResponse = await fetch(
+        plantDiaryId ? `https://i11b308.p.ssafy.io/api/user/diary/${plantDiaryId}` : 'https://i11b308.p.ssafy.io/api/user/diary',
         {
           method: plantDiaryId ? 'PATCH' : 'POST',
           headers: {
             'Content-Type': 'application/json',
+
           },
           body: JSON.stringify(diaryData),
         }
       );
 
-      // if (!response.ok) {
-      //   throw new Error('다이어리 저장에 실패했습니다.');
-      // }
-      // navigate(`/diary/${date}`, { state: { diaryData } });
-    } catch (error) {
-      console.error('Error:', error);
-      // alert(error.message);
-    }
-    
-    // 식물 정보 저장 요청
-    try {
-      const method = plantDiaryId? 'PATCH' : 'POST';
-      const response = await fetch(`/api/user/plant/${plantId}/check`, {
-        method,
+      if (!diaryWriteResponse.ok) throw new Error('일지 저장에 실패했습니다.');
+
+      const plantCheckResponse = await fetch(`https://i11b308.p.ssafy.io/api/user/plant/${plantId}/check`, {
+        method: plantDiaryId ? 'PATCH' : 'POST',
         headers: {
           'Content-Type': 'application/json',
+
         },
         body: JSON.stringify(plantData),
       });
 
-      // if (!response.ok) {
-      //   throw new Error('식물 정보 저장에 실패했습니다.');
-      // }
+      if (!plantCheckResponse.ok) {
+        throw new Error('식물 정보 저장에 실패했습니다.')
+      };
 
+      // state 는 임시로 한거고 나중에는 plantDiaryId로 확인할거임
+      navigate(`/diary/${date}`, {
+        state: {
+          date,
+          content,
+          weather,
+          temperature,
+          humidity,
+          isWatered,
+          isFertilized,
+          isRepotted,
+          imgs,
+        },
+      });
     } catch (error) {
       console.error('Error:', error);
       alert(error.message);
     }
-
-    // TODO state 는 임시로 한거고 나중에는 plantDiaryId로 확인할거임
-    navigate(`/diary/${date}`, 
-      { state: { 
-      date, 
-      content,
-      weather, 
-      temperature, 
-      humidity, 
-      isWatered, 
-      isFertilized, 
-      isRepotted, 
-      imgs
-     } });
-     
   };
+  //   // 일지 작성 요청
+  //   try {
+  //     const diaryWriteResponse = await fetch(
+  //       plantDiaryId ? `/api/user/diary/${plantDiaryId}` : '/api/user/diary', 
+  //       {
+  //         method: plantDiaryId ? 'PATCH' : 'POST',
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //         },
+  //         body: JSON.stringify(diaryData),
+  //       }
+  //     );
+
+  //     if (!diaryWriteResponse.ok) {
+  //       throw new Error('일지 저장에 실패했습니다.');
+  //     }
+  //     if (diaryWriteResponse.ok) {
+  //       console.log('일지 저장 성공');
+  //     }
+  //     // navigate(`/diary/${date}`, { state: { diaryData } });
+  //   } catch (error) {
+  //     console.error('Error:', error);
+  //     // alert(error.message);
+  //   }
+    
+  //   // 식물 정보 저장 요청
+  //   try {
+  //     const method = plantDiaryId? 'PATCH' : 'POST';
+  //     const response = await fetch(`/api/user/plant/${plantId}/check`, {
+  //       method,
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify(plantData),
+  //     });
+
+  //     if (!response.ok) {
+  //       throw new Error('식물 정보 저장에 실패했습니다.');
+  //     }
+
+  //     if (response.ok) {
+  //       console.log('식물 저장 성공');
+  //     }
+
+  //   } catch (error) {
+  //     console.error('Error:', error);
+  //     alert(error.message);
+  //   }
+
+  //   // TODO state 는 임시로 한거고 나중에는 plantDiaryId로 확인할거임
+  //   navigate(`/diary/${date}`, 
+  //     { state: { 
+  //     date, 
+  //     content,
+  //     weather, 
+  //     temperature, 
+  //     humidity, 
+  //     isWatered, 
+  //     isFertilized, 
+  //     isRepotted, 
+  //     imgs
+  //    } });
+     
+  // };
 
   return (
     <div className="plant-diary-container">
