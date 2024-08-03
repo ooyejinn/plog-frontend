cb21a52c847cc457235153734d38fe4744187f63
YejinOh <ooyejinn@gmail.com>
2024-08-03 14:50:49 +0900

    feat: 헤더, 푸터, 코멘트푸터 컴포넌트 뼈대 추가

---
+++ src/components/Common/Footer.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+
+const Footer = () => {
+
+  return (
+    <div>
+      <button>SNS 전체공개 탐색 페이지</button>
+      <button>본인의 프로필 페이지 - 식물관리 탭</button>
+      <button>홈화면 페이지</button>
+      <button>채팅 리스트 페이지</button>
+      <button>알람</button>
+    </div>
+  )
+}
+
+export default Footer;
