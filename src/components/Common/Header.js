9dd4ee3c29b1fb8dc63ad2f7ebb6c928b815ff40
YejinOh <ooyejinn@gmail.com>
2024-08-03 15:28:51 +0900

    feat: 뒤로가기 버튼 추가했으나 test 해봐야 함

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import HeaderBackBtn from './HeaderBackBtn';
+import HeaderMenuBtn from './HeaderMenuBtn';
 
 const Header = () => {
 
-  // TODO: PWA학습 후 뒤로가기 구현 (PWA의 스택 활용 이해 필요)
-
   return (
     <div>
-      <button>뒤로가기</button>
+      <HeaderBackBtn />
       <i>로고</i>
-      <button>햄버거 메뉴</button>
+      <HeaderMenuBtn />
     </div>
   )
 }
