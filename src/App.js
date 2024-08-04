075c4a4192563309b38e2b71c1d96777fdee8418
YejinOh <ooyejinn@gmail.com>
2024-08-05 00:25:53 +0900

    feater: header 기능 추가

--- src/App.js
+++ src/App.js
@@ -5,27 +5,32 @@ import PlantDiaryDetail from './pages/Diary/PlantDiaryDetail';
 import PlantDetail from './pages/Plant/PlantDetail';
 import PlantRegister from './pages/Plant/PlantRegister';
 import ApiTest from './pages/Plant/ApiTest';
-// import Header from './components/Common/Header';
+import Header from './components/Common/Header';
 
 function App() {
   return (
-    <div className='container'>
-      <header className='full-width'>
-        {/* <Header /> */}
-      </header>
-      <main class='content'>
-        <Router>
-          <Routes>
-            {/* <Route path="/plant/:plantId" element={<PlantDetail />} /> */}
-            <Route path="/plant" element={<PlantDetail />} />
-            {/* <Route path="/plant/register/:plantId" element={<PlantDetail />} /> */}
-            <Route path="/plant/register" element={<PlantRegister />} />
-            {/* API TEST 용 */}
-            {/* <Route path="/" element={<ApiTest />} /> */}
-          </Routes>
-        </Router>
-      </main>
-    </div>
+    <Router>
+
+      <div className='container'>
+
+        <header className='full-width'>
+          <Header />
+        </header>
+
+        <main className='content'>
+            <Routes>
+              {/* <Route path="/plant/:plantId" element={<PlantDetail />} /> */}
+              <Route path="/plant" element={<PlantDetail />} />
+              {/* <Route path="/plant/register/:plantId" element={<PlantDetail />} /> */}
+              <Route path="/plant/register" element={<PlantRegister />} />
+              {/* API TEST 용 */}
+              {/* <Route path="/" element={<ApiTest />} /> */}
+            </Routes>
+        </main>
+
+      </div>
+
+    </Router>
   );
 }
 
