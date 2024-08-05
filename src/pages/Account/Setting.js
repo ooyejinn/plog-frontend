a660bbd68e74652d901a97a2f919eccaee7a9152
진아영 <ay8160@daum.net>
2024-08-05 21:09:59 +0900

    feat: 회원탈퇴 구현

--- src/pages/Account/Setting.js
+++ src/pages/Account/Setting.js
@@ -7,6 +7,7 @@ import ModalConfirm from "../../components/Common/ModalConfirm";
 const Setting = () => {
   const navigate = useNavigate();
   const [showLogoutConfirm, setShowLogoutConfirm] = useState(false);
+  const [showSignoutConfirm, setShowSignoutConfirm] = useState(false);
   const clearToken = useAuthStore((state) => state.clearToken);
 
   // 로그아웃
@@ -30,6 +31,27 @@ const Setting = () => {
 
     } catch (error) {
       console.error('로그아웃 실패:', error);
+    }
+  };
+
+  // 회원 탈퇴
+  const handleSignOut = async () => {
+    try {
+      const accessToken = useAuthStore.getState().accessToken;
+      if (!accessToken) {
+        console.log('로그인되지 않은 상태입니다.');
+        return;
+      }
+
+      // 서버에 회원탈퇴 요청
+      const response = await API.delete('/user');
+      console.log('회원 탈퇴 성공:', response.data);
+
+      // 토큰 제거
+      clearToken();
+      navigate('/login');
+    } catch (error) {
+      console.error('회원 탈퇴 실패:', error);
     }
   };
 
@@ -61,8 +83,19 @@ const Setting = () => {
       </section>
       <section className="settings-section">
         <h2>회원탈퇴</h2>
-        <div className="settings-item">탈퇴하기</div>
+        <div
+          className="settings-item"
+          onClick={()=> setShowSignoutConfirm(true)}
+        >탈퇴하기</div>
       </section>
+      <ModalConfirm
+        open={showSignoutConfirm}
+        title="회원탈퇴"
+        content="정말로 탈퇴 하시겠습니까?"
+        confirmText="탈퇴하기"
+        onConfirm={handleSignOut}
+        onClose={() => setShowSignoutConfirm(false)}
+      />
       <ModalConfirm
         open={showLogoutConfirm}
         title="로그아웃"
