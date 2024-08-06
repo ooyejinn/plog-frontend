c9632109d75afff39ceaf03803121f1a0ffe212a
진아영 <ay8160@daum.net>
2024-08-06 14:16:58 +0900

    feat: footer 아이콘 적용

--- src/components/Common/Footer.js
+++ src/components/Common/Footer.js
@@ -1,7 +1,15 @@
-
 import { useNavigate } from "react-router-dom";
 import useButtonStore from "../../stores/footer";
-import snsSelect from "../../assets/icon/footer/sns-select.png"
+import snsSelect from "../../assets/icon/footer/sns-select.png";
+import snsDefault from "../../assets/icon/footer/sns-default.png";
+import profileSelect from "../../assets/icon/footer/profile-select.png";
+import profileDefault from "../../assets/icon/footer/profile-default.png";
+import homeSelect from "../../assets/icon/footer/home-select.png";
+import homeDefault from "../../assets/icon/footer/home-default.png";
+import chatSelect from "../../assets/icon/footer/chat-select.png";
+import chatDefault from "../../assets/icon/footer/chat-default.png";
+import alarmSelect from "../../assets/icon/footer/alarm-select.png";
+import alarmDefault from "../../assets/icon/footer/alarm-default.png";
 
 const Footer = () => {
   const { activeButton, setActiveButton } = useButtonStore(); 
@@ -34,29 +42,34 @@ const Footer = () => {
   return (
     <div>
       <img
-        src={activeButton === "sns" ? "../../assets/icon/footer/sns-select.png" : "../../assets/icon/footer/sns-default.png"}
+        src={activeButton === "sns" ? snsSelect : snsDefault}
         alt="sns 아이콘"
         onClick={() => handleClick("sns")}
+        style={{ width: '50px', height: '50px'}}
       />
       <img
-        src={activeButton === "profile" ? "../../assets/icon/footer/profile-select.png" : "../../assets/icon/footer/profile-default.png"}
+        src={activeButton === "profile" ? profileSelect : profileDefault}
         alt="profile 아이콘"
         onClick={() => handleClick("profile")}
+        style={{ width: '50px', height: '50px'}}
       />
       <img
-        src={activeButton === "home" ? "../../assets/icon/footer/home-select.png" : "../../assets/icon/footer/home-default.png"}
+        src={activeButton === "home" ? homeSelect : homeDefault}
         alt="home 아이콘"
         onClick={() => handleClick("home")}
+        style={{ width: '50px', height: '50px'}}
       />
       <img
-        src={activeButton === "chat" ? "../../assets/icon/footer/chat-select.png" : "../../assets/icon/footer/chat-default.png"}
+        src={activeButton === "chat" ? chatSelect : chatDefault}
         alt="chat 아이콘"
         onClick={() => handleClick("chat")}
+        style={{ width: '50px', height: '50px'}}
       />
       <img
-        src={activeButton === "alarm" ? "../../assets/icon/footer/alarm-select.png" : "../../assets/icon/footer/alarm-default.png"}
+        src={activeButton === "alarm" ? alarmSelect : alarmDefault}
         alt="alarm 아이콘"
         onClick={() => handleClick("alarm")}
+        style={{ width: '50px', height: '50px'}}
       />
     </div>
   );
