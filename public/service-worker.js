4208cc3ff4353b49e1616fc9177458e95da3adc1
이영원 <dlduddnjs198@naver.com>
2024-08-07 17:49:49 +0900

    refactor: Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,22 +1,29 @@
-const CACHE_NAME = 'pwa-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192x192.png',
-  '/icon-512x512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+/* eslint-disable no-undef */
+self.addEventListener('install', (event) => {
+  // 서비스 워커가 설치될 때 즉시 활성화되도록 설정
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('activate', (event) => {
+  // 서비스 워커가 활성화될 때 기존 캐시를 모두 삭제
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // 모든 네트워크 요청에 대해 캐시를 사용하지 않고 직접 네트워크에서 응답
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request).catch(() => {
+      // 네트워크 요청이 실패했을 때 (오프라인일 때 등)
+      return new Response('Network request failed and no cache available.');
+    })
   );
 });
