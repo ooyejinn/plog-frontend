c9c3368efdf6c6bdebe1ae381adc84e81d83b2df
오예진 <ooyejinn@gmail.com>
2024-08-08 10:49:09 +0900

    fix: 깃충돌수정

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,22 +1,15 @@
-const CACHE_NAME = 'pwa-cache-v1';
-const urlsToCache = [
-  // '/',
-  // '/index.html',
-  // '/manifest.json'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+// service-worker.js
+// 서비스 워커 설치 이벤트
+self.addEventListener("install", (event) => {
+  self.skipWaiting(); // 새 서비스 워커가 즉시 활성화되도록 합니다.
 });
 
+// 서비스 워커 활성화 이벤트
+self.addEventListener("activate", (event) => {
+  self.clients.claim(); // 새 서비스 워커가 즉시 활성화되도록 합니다.
+});
 
-//fetch 이벤트에서 캐시된 파일 제공
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+// 요청 가로채기 이벤트
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
 });
