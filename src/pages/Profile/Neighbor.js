2df4f9f77d450eafc95081fb31cd3a784f6e0f68
오예진 <ooyejinn@gmail.com>
2024-08-08 17:48:05 +0900

    feat: 이웃 리스트 출력 api test

--- src/pages/Profile/Neighbor.js
+++ src/pages/Profile/Neighbor.js
@@ -1,8 +1,41 @@
-import react, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import API from '../../apis/api';
 
 const Neighbor = () => {
 
   const [activeTab, setActiveTab] = useState('follow');
+  const [followList, setFollowList] = useState([]);
+  const [followerList, setFollowerList] = useState([]);
+  const { searchId } = useParams();
+
+  const fetchFollowList = async () => {
+    try {
+      const response = await API.get(`/user/neighbor/${searchId}/to`)
+      setFollowList(response.data);
+      console.log('followList', followList)
+    } catch (error) {
+      console.error("팔로우 목록 에러", error);
+    }
+  }
+
+  const fetchFollowerList = async () => {
+    try {
+      const response = await API.get(`/user/neighbor/${searchId}/from`)
+      setFollowerList(response.data);
+      console.log('followerList', followList)
+    } catch (error) {
+      console.error("팔로워 목록 에러", error);
+    }
+  }
+
+  useEffect(() => {
+    if (activeTab === 'follow') {
+      fetchFollowList();
+    } else {
+      fetchFollowerList();
+    }
+  }, [activeTab, searchId]);
 
   return (
     <div>
@@ -11,7 +44,26 @@ const Neighbor = () => {
         <button className={activeTab === 'follow' ? 'active' : ''} onClick={() => setActiveTab('follow')}>팔로우</button>
         <button className={activeTab === 'follower' ? 'active' : ''} onClick={() => setActiveTab('follower')}>팔로워</button>
       </div>
-
+      
+      {activeTab === 'follow' && (
+        <>
+          <h5>팔로우 목록</h5>
+          <ul>
+            {followList.map((user) => (
+              <li key={user.id}>
+                <img src={user.profileImage} alt={`${user.nickname} profile`} width="50" height="50" />
+                <span>{user.nickname}</span>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+      
+      {activeTab === 'follower' && (
+        <>
+          <h5>팔로워 목록</h5>
+        </>
+      )}
     </div>
   )
 }
