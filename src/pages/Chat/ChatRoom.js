3fcbeb4c2058fee45f1fe1441c0f5adf593014bd
강윤서 <ysys1130@gmail.com>
2024-08-10 15:03:22 +0900

    feat: websocket 연결

--- src/pages/Chat/ChatRoom.js
+++ src/pages/Chat/ChatRoom.js
@@ -1,22 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import SockJS from 'sockjs-client';
 import { Client } from '@stomp/stompjs';
-import './ChatRoom.css'; // CSS 스타일을 이 파일에 저장
 
-const API_REALTIME_URL = "https://i11b308.p.ssafy.io/realtime";
+const API_REALTIME_URL = "https://i11b308.p.ssafy.io/realtime"; // realtime api 주소
+// 1. 사용자를 식별할 JWT -> cookie에서 가져오기
 const token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiaXNzIjoicGxvZy5jb20iLCJleHAiOjE3MjQwNDUzMTUsImlhdCI6MTcyMjgzNTcxNX0.BFVSXUtidgN3jrayov5V0wnDeU2QkItay86uQu2wf3o";
 
-const ChatRoom = ({ chatRoomId }) => {
-  const [client, setClient] = useState(null);
+const ChatRoom = ({ chatRoomId }) => { // ChatRoomList에서 해당 채팅방을 클릭하면 chatRoomId를 가지고 페이지 이동
+  const [client, setClient] = useState(null); // stomp client
   const [messages, setMessages] = useState([]);
   const [user, setUser] = useState({
-    nickname: "nickname",
-    profile: "https://plogbucket.s3.ap-northeast-2.amazonaws.com/free-icon-sprout-267205.png",
+    nickname: "nickname", // cookie에서 가져올 로그인한 회원 닉네임
+    profile: "https://plogbucket.s3.ap-northeast-2.amazonaws.com/free-icon-sprout-267205.png", // cookie에서 가져올 로그인한 회원 프로필 사진
   });
   const [messageContent, setMessageContent] = useState("");
 
   useEffect(() => {
-    const socket = new SockJS(`${API_REALTIME_URL}/chat/ws`);
+    const socket = new SockJS(`${API_REALTIME_URL}/chat/ws`); // sockjs 를 이용한 websocket 연결
 
     const stompClient = new Client({
       webSocketFactory: () => socket,
@@ -25,14 +25,14 @@ const ChatRoom = ({ chatRoomId }) => {
       },
       onConnect: (frame) => {
         console.log('WebSocket 연결 성공: ' + frame);
-        stompClient.subscribe('/topic/public', messageOutput => {
-          showMessage(JSON.parse(messageOutput.body));
+        stompClient.subscribe('/topic/chatroom-2', messageOutput => { // chatroom-${chatRoomId} 가 돼야 함! 
+          showMessage(JSON.parse(messageOutput.body)); // subscriber들이 받을 publish 된 메시지 띄워주는 함수
         });
 
         console.log('Join 메시지 전송');
         stompClient.publish({
-          destination: `/app/chat.addUser/${chatRoomId}`,
-          headers: { 'Authorization': token },
+          destination: `/app/chat.addUser/2`, // /add.chat.addUser/${chatRoomId} 가 돼야 함
+          headers: { 'Authorization': token }, // 토큰 가지고 넘기기
           body: JSON.stringify({
             nickname: user.nickname,
             profile: user.profile,
@@ -40,13 +40,6 @@ const ChatRoom = ({ chatRoomId }) => {
             chatType: "JOIN",
             createdAt: null
           })
-        });
-        showMessage({
-          nickname: user.nickname,
-          profile: user.profile,
-          message: `${user.nickname}님이 들어오셨습니다.`,
-          chatType: "JOIN",
-          createdAt: null
         });
       },
       onStompError: (frame) => {
@@ -70,10 +63,9 @@ const ChatRoom = ({ chatRoomId }) => {
 
     console.log("보내는 메시지: " + messageContent);
     console.log('Send 메시지 전송');
-    console.log(`${chatRoomId}`)
     if (client && client.connected) {
       client.publish({
-        destination: `/app/chat.sendMessage/1`,
+        destination: `/app/chat.sendMessage/2`, // 2 대신 chatRoomId
         headers: { 'Authorization': token },
         body: JSON.stringify({
           nickname: user.nickname,
@@ -85,20 +77,13 @@ const ChatRoom = ({ chatRoomId }) => {
       });
     }
     setMessageContent("");
-    showMessage({
-      nickname: user.nickname,
-      profile: user.profile,
-      message: messageContent,
-      chatType: "SEND",
-      createdAt: null
-    });
   };
 
   const leaveChat = (client) => {
     if (client && client.connected) {
       console.log('Leave 메시지 전송');
       client.publish({
-        destination: `/app/chat.leaveUser/1`,
+        destination: `/app/chat.leaveUser/2`, // 2 대신 chatRoomId
         headers: { 'Authorization': token },
         body: JSON.stringify({
           nickname: user.nickname,
@@ -108,13 +93,6 @@ const ChatRoom = ({ chatRoomId }) => {
           createdAt: null
         })
       });
-      showMessage({
-        nickname: user.nickname,
-        profile: user.profile,
-        message: `${user.nickname}님이 나갔습니다.`,
-        chatType: "LEAVE",
-        createdAt: null
-      });
     }
   };
 
@@ -123,11 +101,6 @@ const ChatRoom = ({ chatRoomId }) => {
     setMessages(prevMessages => [...prevMessages, message]);
   };
 
-  const getMessageColor = (message) => {
-    const colors = ["blue-message", "purple-message", "orange-message"];
-    const index = message.nickname.length % colors.length;
-    return colors[index];
-  };
 
   return (
     <div className="chat-container">
@@ -136,7 +109,7 @@ const ChatRoom = ({ chatRoomId }) => {
       </div>
       <div className="chat-box" id="response">
         {messages.map((message, index) => (
-          <div key={index} className={message.chatType === "SEND" ? `send-message ${getMessageColor(message)}` : message.chatType === "JOIN" ? "join-message" : "leave-message"}>
+          <div key={index}>
             {message.chatType === "SEND" && <img src={message.profile} className="profile-img" alt="Profile Image" />}
             <div className="message-content">
               <div className="nickname">{message.nickname}</div>
