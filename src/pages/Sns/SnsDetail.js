a85f296a80521191fe9ac04dc1cc976a784f4b3c
진아영 <ay8160@daum.net>
2024-08-14 17:25:18 +0900

    fix: 시간 수정

--- src/pages/Sns/SnsDetail.js
+++ src/pages/Sns/SnsDetail.js
@@ -20,6 +20,17 @@ const SnsDetail = () => {
 
   const { userData } = useAuthStore();
 
+  // 시간
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 +1
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    return `${year}.${month}.${day} ${hours}:${minutes}`;
+  }
+
   useEffect(() => {
     if (!articleId) {
       console.error('@@@Invalid access: No articleId found.');
@@ -34,7 +45,7 @@ const SnsDetail = () => {
         setArticle(response.data);
         const writerInfo = {
           profile: response.data.profile,
-          recordDate: response.data.createdAt.slice(0, 10),
+          recordDate: formatDate(response.data.createdAt),
           nickname: response.data.nickname,
           searchId: response.data.searchId,
         }
@@ -70,6 +81,7 @@ const SnsDetail = () => {
     navigate(`/profile/${writerInfo.searchId}`)
     console.log(writerInfo)
   }
+
 
   // 모달 열기
   const openDeleteModal = () => {
